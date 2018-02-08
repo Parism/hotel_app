c79bc9ea0c5b553dff522409a81119999542d1d1
Paris Manolas <nool.manolas@gmail.com>
2018-02-08 19:09:40 +0200

    Initial commit

---
+++ app/src/main/java/com/example/hotel/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.hotel;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import com.facebook.FacebookSdk;
+import com.facebook.appevents.AppEventsLogger;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
