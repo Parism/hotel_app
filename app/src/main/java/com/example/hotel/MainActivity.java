57f83ff53d8bebb68275557c9ba535bb2e30372d
Thanasis17m <thanasis17m@yahoo.com>
2018-02-11 16:11:40 +0200

    Changed main activity logo

--- app/src/main/java/com/example/hotel/MainActivity.java
+++ app/src/main/java/com/example/hotel/MainActivity.java
@@ -2,8 +2,7 @@ package com.example.hotel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.facebook.FacebookSdk;
-import com.facebook.appevents.AppEventsLogger;
+import android.support.v7.widget.AppCompatImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,5 +10,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final AppCompatImageView icAndroid = (AppCompatImageView) findViewById(R.id.imageView);
+        icAndroid.setImageResource(R.drawable.ic_hotel_logo);
     }
 }
