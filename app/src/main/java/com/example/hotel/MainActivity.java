cb953b33f7732acfecd7ddfbe9cf0fa9e228d0f8
Thanasis17m <thanasis17m@yahoo.com>
2018-02-13 00:19:48 +0200

    Removed useless svg code.

--- app/src/main/java/com/example/hotel/MainActivity.java
+++ app/src/main/java/com/example/hotel/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.hotel;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.v7.widget.AppCompatImageView;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +11,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final AppCompatImageView icAndroid = (AppCompatImageView) findViewById(R.id.imageView);
-        icAndroid.setImageResource(R.drawable.ic_hotel_logo);
     }
 }
