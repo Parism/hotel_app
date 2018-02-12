cb4ebb6d1eb8153dfd69407e66b6af52c9dfa4ed
Paris Manolas <nool.manolas@gmail.com>
2018-02-12 19:47:13 +0200

    Implemented signup text link to open signup activity (listener), changed some id's and made style changes (colors etc.)

--- app/src/main/java/com/example/hotel/MainActivity.java
+++ app/src/main/java/com/example/hotel/MainActivity.java
@@ -1,17 +1,38 @@
 package com.example.hotel;
 
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.AppCompatImageView;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private TextView signupTextLink;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final AppCompatImageView icAndroid = (AppCompatImageView) findViewById(R.id.imageView);
+        final AppCompatImageView icAndroid = (AppCompatImageView) findViewById(R.id.hotelLogo);
         icAndroid.setImageResource(R.drawable.ic_hotel_logo);
+
+        signupTextLink = (TextView) findViewById(R.id.signupText);
+
+        signupTextLink.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                signupTextLink.setTextColor(Color.BLACK);
+                Context context = MainActivity.this;
+                Class destinationActivity = SignUpActivity.class;
+                Intent startSignupActivityIntent = new Intent(context, destinationActivity);
+                startActivity(startSignupActivityIntent);
+
+            }
+        });
+
     }
 }
