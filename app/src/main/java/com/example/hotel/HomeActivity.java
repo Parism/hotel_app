268a7cc2dc6987d3237d3379d93a4b9da5fec048
Thanasis17m <thanasis17m@yahoo.com>
2018-03-14 00:32:43 +0200

    Drawer layout and functionality moved to home activity instead of home fragment.Removed link account drawer option.

--- app/src/main/java/com/example/hotel/HomeActivity.java
+++ app/src/main/java/com/example/hotel/HomeActivity.java
@@ -1,15 +1,26 @@
 package com.example.hotel;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.net.Uri;
 import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
 import android.support.v4.view.ViewPager;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.view.MenuItem;
+import android.widget.Toast;
 import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-
 import java.util.Arrays;
+import java.util.List;
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -20,9 +31,45 @@ public class HomeActivity extends AppCompatActivity {
     private FirebaseAuth.AuthStateListener mAuthStateListener;
     private static final int RC_SIGN_IN = 1;
 
+
     public void setCurrentItem (int item, boolean smoothScroll) { //scrolls from the current fragment to the item fragment
         viewPager.setCurrentItem(item, smoothScroll);
     }
+
+
+    private static Intent newFacebookIntent(PackageManager pm, String url) { //creates an intent to the facebook page
+        Uri uri = Uri.parse(url);
+        try {
+            ApplicationInfo applicationInfo = pm.getApplicationInfo("com.facebook.katana", 0);
+            if (applicationInfo.enabled) {
+                // http://stackoverflow.com/a/24547437/1048340
+                uri = Uri.parse("fb://facewebmodal/f?href=" + url);
+            }
+        } catch (PackageManager.NameNotFoundException ignored) {
+        }
+        return new Intent(Intent.ACTION_VIEW, uri);
+    }
+
+
+    private boolean isIntentAvailable(Context ctx, Intent intent) {
+        final PackageManager packageManager = ctx.getPackageManager();
+        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
+        return list.size() > 0;
+    }
+
+    private void newInstagramIntent(Context activity, String url) {   //creates an intent to the instagram page
+        Uri uri = Uri.parse(url);
+        Intent insta = new Intent(Intent.ACTION_VIEW, uri);
+        insta.setPackage("com.instagram.android");
+
+        if (isIntentAvailable(activity, insta)) {
+            startActivity(insta);
+        } else {
+            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
+        }
+    }
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +78,11 @@ public class HomeActivity extends AppCompatActivity {
 
         mFirebaseAuth = FirebaseAuth.getInstance();
 
-
-
         mAuthStateListener = new FirebaseAuth.AuthStateListener() {
             @Override
             public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
                 FirebaseUser user = firebaseAuth.getCurrentUser();
                 if(user != null){
-
 
                 }
                 else{
@@ -67,10 +111,54 @@ public class HomeActivity extends AppCompatActivity {
         // Set the adapter onto the view pager
         viewPager.setAdapter(adapter);
 
+        final DrawerLayout mDrawerLayout = this.findViewById(R.id.menu_drawer);
 
+        NavigationView navigationView = this.findViewById(R.id.menu);
+        navigationView.setNavigationItemSelectedListener(                   //Handle the drawer options
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                        int id = menuItem.getItemId();
+
+                        if (id == R.id.nav_like_facebook) { //opens an intent to the fb page
+                            Intent fbIntent = newFacebookIntent(getPackageManager(),"https://www.facebook.com/antonysuites/");
+                            startActivity(fbIntent);
+
+                        } else if(id == R.id.nav_follow_instagram){ //opens an intent to the insta page
+                            newInstagramIntent(getApplicationContext(), "http://instagram.com/_u/antony_suites");
+                        }
+
+                        else if(id == R.id.nav_logout_account){
+                            AuthUI.getInstance().signOut(getApplicationContext());
+                        }
+
+                        else if(id == R.id.nav_delete_account){
+                            AuthUI.getInstance()
+                                    .delete(getApplicationContext())
+                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                                        @Override
+                                        public void onComplete(@NonNull Task<Void> task) {
+                                            if (task.isSuccessful()) {
+                                                Toast.makeText(getApplicationContext(),"Account deleted",Toast.LENGTH_SHORT).show();
+                                            } else {
+                                                Toast.makeText(getApplicationContext(),"Deletion failed",Toast.LENGTH_SHORT).show();
+                                            }
+                                        }
+                                    });
+                        }
+
+
+                        mDrawerLayout.closeDrawers();   // close drawer when item is tapped
+
+                        return true;
+                    }
+                });
 
 
     }
+
+
 
     @Override
     protected void onPause() {
@@ -78,12 +166,12 @@ public class HomeActivity extends AppCompatActivity {
         mFirebaseAuth.removeAuthStateListener(mAuthStateListener);
     }
 
+
     @Override
     protected void onResume() {
         super.onResume();
         mFirebaseAuth.addAuthStateListener(mAuthStateListener);
     }
-
 
 
 
--- app/src/main/java/com/example/hotel/HomeFragment.java
+++ app/src/main/java/com/example/hotel/HomeFragment.java
@@ -1,73 +1,21 @@
 package com.example.hotel;
 
-import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ApplicationInfo;
-import android.content.pm.PackageManager;
-import android.content.pm.ResolveInfo;
-import android.media.Image;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
-import android.support.design.widget.NavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.firebase.ui.auth.AuthUI;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-
-import java.util.List;
 
 
 public class HomeFragment extends Fragment {
-
-
-    private static Intent newFacebookIntent(PackageManager pm, String url) { //creates an intent to the facebook page
-        Uri uri = Uri.parse(url);
-        try {
-            ApplicationInfo applicationInfo = pm.getApplicationInfo("com.facebook.katana", 0);
-            if (applicationInfo.enabled) {
-                // http://stackoverflow.com/a/24547437/1048340
-                uri = Uri.parse("fb://facewebmodal/f?href=" + url);
-            }
-        } catch (PackageManager.NameNotFoundException ignored) {
-        }
-        return new Intent(Intent.ACTION_VIEW, uri);
-    }
-
-
-
-    private boolean isIntentAvailable(Context ctx, Intent intent) {
-        final PackageManager packageManager = ctx.getPackageManager();
-        List<ResolveInfo> list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY);
-        return list.size() > 0;
-    }
-
-    private void newInstagramIntent(Context activity, String url) {   //creates an intent to the instagram page
-
-        Uri uri = Uri.parse(url);
-        Intent insta = new Intent(Intent.ACTION_VIEW, uri);
-        insta.setPackage("com.instagram.android");
-
-        if (isIntentAvailable(activity, insta)) {
-            startActivity(insta);
-        } else {
-            startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse(url)));
-        }
-    }
-
-
 
 
     @Override
@@ -75,7 +23,6 @@ public class HomeFragment extends Fragment {
 
         View homeView = inflater.inflate(R.layout.fragment_home, container, false);
 
-        final DrawerLayout mDrawerLayout = homeView.findViewById(R.id.menu_drawer);
 
         Toolbar toolbar =  homeView.findViewById(R.id.homeToolbar); //toolbar creation
         final AppCompatActivity activity = (AppCompatActivity) getActivity(); //find current activity
@@ -84,6 +31,9 @@ public class HomeFragment extends Fragment {
         if(activity.getSupportActionBar() != null){
             activity.getSupportActionBar().setDisplayShowTitleEnabled(false); //hide app title from toolbar
         }
+
+
+        final DrawerLayout mDrawerLayout = activity.findViewById(R.id.menu_drawer);
 
         ImageView menu_icon = homeView.findViewById(R.id.action_menu);
         ImageView chat_icon = homeView.findViewById(R.id.action_chat);
@@ -118,60 +68,6 @@ public class HomeFragment extends Fragment {
 
 
 
-       NavigationView navigationView = homeView.findViewById(R.id.menu);
-        navigationView.setNavigationItemSelectedListener(                   //Handle the drawer options
-               new NavigationView.OnNavigationItemSelectedListener() {
-                    @Override
-                    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                        // set item as selected to persist highlight
-                        //menuItem.setChecked(true);
-
-                        int id = menuItem.getItemId();
-
-                        if (id == R.id.nav_like_facebook) { //opens an intent to the fb page
-                            Intent fbIntent = newFacebookIntent(activity.getPackageManager(),"https://www.facebook.com/antonysuites/");
-                            startActivity(fbIntent);
-
-                        } else if(id == R.id.nav_follow_instagram){ //opens an intent to the insta page
-                            newInstagramIntent(activity, "http://instagram.com/_u/antony_suites");
-
-                        }
-
-                        else if(id == R.id.nav_logout_account){
-                            AuthUI.getInstance().signOut(activity.getApplicationContext());
-
-                        }
-
-                        else if(id == R.id.nav_delete_account){
-                            AuthUI.getInstance()
-                                    .delete(activity.getApplicationContext())
-                                    .addOnCompleteListener(new OnCompleteListener<Void>() {
-                                        @Override
-                                        public void onComplete(@NonNull Task<Void> task) {
-                                            if (task.isSuccessful()) {
-                                                Toast.makeText(activity.getApplicationContext(),"Account deleted",Toast.LENGTH_SHORT).show();
-                                            } else {
-                                                // Deletion failed
-                                            }
-                                        }
-                                    });
-                        }
-
-
-
-                        // close drawer when item is tapped
-                        mDrawerLayout.closeDrawers();
-
-                        // Add code here to update the UI based on the item selected
-                        // For example, swap UI fragments here
-
-                        return true;
-                    }
-                });
-
-
-
-
         homeView.setOnTouchListener(new OnSwipeTouchListener(activity) {  //Handles swipes
             public void onSwipeTop() {
                 //Toast.makeText(activity, "top", Toast.LENGTH_SHORT).show();
@@ -190,25 +86,25 @@ public class HomeFragment extends Fragment {
         });
 
 
-        ViewPager viewPager = activity.findViewById(R.id.viewpager);
-        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() { //Handles fragment page change
-            @Override
-            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
-
-            }
-
-            @Override
-            public void onPageSelected(int position) {  //if chat fragment is selected,the navigation drawer is closed
-                if(position == 1) {
-                    mDrawerLayout.closeDrawers();
-                }
-            }
-
-            @Override
-            public void onPageScrollStateChanged(int state) {
-
-            }
-        });
+//        ViewPager viewPager = activity.findViewById(R.id.viewpager);
+//        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() { //Handles fragment page change
+//            @Override
+//            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+//
+//            }
+//
+//            @Override
+//            public void onPageSelected(int position) {  //if chat fragment is selected,the navigation drawer is closed
+//                if(position == 1) {
+//                    mDrawerLayout.closeDrawers();
+//                }
+//            }
+//
+//            @Override
+//            public void onPageScrollStateChanged(int state) {
+//
+//            }
+//        });
 
 
 
